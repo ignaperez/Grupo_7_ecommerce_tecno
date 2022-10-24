d52013a2a97205fc3c065a912e902a01722e5bca
Jcmias25 <jerecuenca@hotmail.com>
2022-10-24 18:12:11 -0300

    arme la logica del registro, pero hay un problema cuando queres cargar la imagen

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
 const path = require("path");
 const userFilePath = path.join(__dirname, '../data/users.json');
-const productos = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
-const multer = require("multer");
+const users = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+/* const productos = JSON.parse(fs.readFileSync(userFilePath, 'utf-8')); */
+ const multer = require("multer");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -14,16 +15,32 @@ const storage = multer.diskStorage({
     }
 })
 
-var upload = multer({storage});
+var upload = multer({storage: storage}); 
+
 const userController = {
     login:(req,res)=> {
         res.render ('login');
 
     },
+
     registro:(req,res)=>{
         res.render ('registro');
 
     },
+
+    logicRegister:(req,res) => {
+        console.log(req.files[0])
+        
+        let newUser = {
+            id : users[users.length - 1].id + 1,
+            're-password': null,
+            ...req.body,
+            categoria: "user",
+        }
+        users.push(newUser);
+        fs.writeFileSync(userFilePath, JSON.stringify(users,null,''));
+        res.redirect('/');
+    }
 }
 
-module.exports = userController
+module.exports = userController;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 
-const storage = multer.diskStorage({
+var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/img/users");
+        cb(null,'public/img/users');
     },
     
     filename: (req, file, cb) => {
@@ -14,11 +14,14 @@ const storage = multer.diskStorage({
     }
 })
 
-var upload = multer({storage});
+var upload = multer({storage: storage});
 
 
 router.get("/login", userController.login);
+
 router.get("/registro", userController.registro)
+router.post("/registro", upload.any(), userController.logicRegister)
 
 
-module.exports = router
+module.exports = router;
+
