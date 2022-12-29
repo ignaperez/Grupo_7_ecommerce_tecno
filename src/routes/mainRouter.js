8fb9ed51d13ba7b1706aaac1611721057aa3d6ee
Ignacio <ignaperez76@gmail.com>
2022-12-29 16:40:05 -0300

    Se arreglo validacion email y nombre de usuario form de registro

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -4,12 +4,15 @@ const express = require('express');
 const mainController = require('../controllers/mainController');
 const userNotLoggedMiddleware = require("../middlewares/userNotLoggedMiddleware");
 const router =express.Router();
+const nodemailer = require('nodemailer');
+const { sendEmail } = require("../email/email");
 
 //RUTAS
 router.get('/',mainController.index); //ruta home
 router.get('/ayuda', mainController.ayuda);
 router.get('/logout',mainController.logout);//logout Cualquier sesion
 router.get('/search', mainController.search)
+router.post('/send-email', mainController.mandaEmail)
 
 
 
